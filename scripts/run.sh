6225e98ab574c52585be0e1e3e69c428d48c65d4
Zikun Wang <terryiochina@gmail.com>
2026-06-29 13:19:05 -0400

    uncomment benchmarks

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,22 +5,22 @@ chmod +x $SCRIPT_DIR/scripts/**/*.sh
 mkdir -p $SCRIPT_DIR/results
 
 # Construction
-# $SCRIPT_DIR/scripts/construction/bigann100M.sh
-# $SCRIPT_DIR/scripts/construction/deep100M.sh
-# $SCRIPT_DIR/scripts/construction/gist1M.sh
-# $SCRIPT_DIR/scripts/construction/openai2M.sh
-# $SCRIPT_DIR/scripts/construction/text2image10M.sh
+$SCRIPT_DIR/scripts/construction/bigann100M.sh
+$SCRIPT_DIR/scripts/construction/deep100M.sh
+$SCRIPT_DIR/scripts/construction/gist1M.sh
+$SCRIPT_DIR/scripts/construction/openai2M.sh
+$SCRIPT_DIR/scripts/construction/text2image10M.sh
 
-# # Query
-# $SCRIPT_DIR/scripts/query/bigann100M.sh
-# $SCRIPT_DIR/scripts/query/deep100M.sh
-# $SCRIPT_DIR/scripts/query/gist1M.sh
-# $SCRIPT_DIR/scripts/query/openai2M.sh
-# $SCRIPT_DIR/scripts/query/text2image10M.sh
+# Query
+$SCRIPT_DIR/scripts/query/bigann100M.sh
+$SCRIPT_DIR/scripts/query/deep100M.sh
+$SCRIPT_DIR/scripts/query/gist1M.sh
+$SCRIPT_DIR/scripts/query/openai2M.sh
+$SCRIPT_DIR/scripts/query/text2image10M.sh
 
-# # Block tuning
-# $SCRIPT_DIR/scripts/block_tuning/bigann100M.sh
-# $SCRIPT_DIR/scripts/block_tuning/openai2M.sh
+# Block tuning
+$SCRIPT_DIR/scripts/block_tuning/bigann100M.sh
+$SCRIPT_DIR/scripts/block_tuning/openai2M.sh
 
 # Incremental construction
 $SCRIPT_DIR/scripts/incremental_construction/bigann100M.sh
